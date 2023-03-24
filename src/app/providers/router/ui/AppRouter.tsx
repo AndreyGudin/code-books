a7542f75a463bb2179243e59239d434dd6acae55
Andrey Gudin <aeon150791@gmail.com>
2023-03-24 18:01:12 +0400

    refactor: move router to diffrent file, add config for routes

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { routeConfig } from "shared/config/routerConfig/routerConfig";
+
+export const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => {
+          return <Route key={path} path={path} element={element} />;
+        })}
+      </Routes>
+    </Suspense>
+  );
+};
