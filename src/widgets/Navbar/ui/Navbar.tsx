078acd30a34c539750052ef02da8174a4a22a8da
Andrey Gudin <aeon150791@gmail.com>
2023-03-26 20:32:29 +0400

    feat: add navbar, applink

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { classNames } from "shared/lib/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+
+import cls from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          className={cls.mainLink}
+          to={"/"}
+        >
+          Главная
+        </AppLink>
+        <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
+          О сайте
+        </AppLink>
+      </div>
+    </div>
+  );
+};
