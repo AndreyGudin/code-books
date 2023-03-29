ffd3212bbcc6905753fb4b13a0c32912b28f0516
Andrey Gudin <aeon150791@gmail.com>
2023-03-29 13:43:43 +0400

    feat: add i18n to project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
-import "./styles/index.scss";
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 
 import { classNames } from "shared/lib/classNames";
 import { useTheme } from "app/providers/ThemeProviders";
@@ -6,15 +7,19 @@ import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 
+import "./styles/index.scss";
+
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
