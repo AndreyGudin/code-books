d11b5990e0573e587952c8fb0d0e1cc2124c2ad9
Andrey Gudin <aeon150791@gmail.com>
2023-03-31 16:10:05 +0400

    feat: add eslint i18next

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,25 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:i18next/recommended',
+    'prettier'
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'i18next'],
   rules: {
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     semi: 0,
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/naming-convention': 'warn'
+    '@typescript-eslint/naming-convention': 'warn',
+    'i18next/no-literal-string': ['error', { markupOnly: true }]
   }
 };
