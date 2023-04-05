ed3995de2bab1a242ae16954514863f40cb091e7
Andrey Gudin <aeon150791@gmail.com>
2023-04-05 16:00:05 +0400

    feat: add error boundary, page error

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import type { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = ({
+  className = ''
+}: PageErrorProps) => {
+  const { t } = useTranslation();
+
+  const reloadPage = (): void => {
+    location.reload();
+  };
+  console.log('err', classNames(cls.PageError, {}, [className]));
+  return (
+    <div className={classNames(cls.PageError, {}, [className])}>
+      <p>{t('Произошла непредвиденная ошибка')}</p>
+      <Button onClick={reloadPage}>{t('Обновить страницу')}</Button>
+    </div>
+  );
+};
