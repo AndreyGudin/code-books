9fee1325942f4b1855b548980ed1ee293e606350
Andrey Gudin <aeon150791@gmail.com>
2023-04-25 13:39:37 +0400

    feat: add redux

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -6,7 +6,7 @@ import 'app/styles/index.scss';
 export const ThemeDecorator: (theme: Theme) => Decorator =
   (theme: Theme) => (StoryComponent) =>
     (
-      <ThemeProvider>
+      <ThemeProvider initialTheme={theme}>
         <div className={`app ${theme}`}> {<StoryComponent />}</div>
       </ThemeProvider>
     );
