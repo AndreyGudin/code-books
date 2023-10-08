0e5d865faf6e8f7512653de810cfc2b7ed614ef0
Andrey Gudin <aeon150791@gmail.com>
2023-10-08 22:02:33 +0400

    feat: add script for clearing features flags

---
+++ src/shared/lib/features/toggleFeatures.ts
@@ -0,0 +1,19 @@
+import { getFeatureFlag } from './setGetFeatures';
+import { FeatureFlags } from '../../types/featureFlags';
+
+interface ToggleFeaturesOptions<T> {
+  name: keyof FeatureFlags;
+  on: () => T;
+  off: () => T;
+}
+
+export function toggleFeatures<T>({
+  name,
+  on,
+  off
+}: ToggleFeaturesOptions<T>): T {
+  if (getFeatureFlag(name)) {
+    return on();
+  }
+  return off();
+}
