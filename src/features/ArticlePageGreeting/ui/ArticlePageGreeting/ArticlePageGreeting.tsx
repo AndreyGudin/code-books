d8cc2cf79e2b36610625c271279b990e0af39248
Andrey Gudin <aeon150791@gmail.com>
2023-10-10 12:44:45 +0400

    feat: add article page greeting

---
+++ src/features/ArticlePageGreeting/ui/ArticlePageGreeting/ArticlePageGreeting.tsx
@@ -0,0 +1,53 @@
+import { useTranslation } from 'react-i18next';
+import { memo, useEffect, useState } from 'react';
+import type { FC } from 'react';
+import { Modal } from '@/shared/ui/Modal';
+import { Text } from '@/shared/ui/Text';
+import { saveJsonSettings, useJsonSettings } from '@/entities/User';
+import { useAppDispatch } from '@/shared/hooks/useAppDispatch';
+import { useDevice } from '@/shared/hooks/useDevice';
+import { Drawer } from '@/shared/ui/Drawer';
+
+export const ArticlePageGreeting: FC = memo(() => {
+  const { t } = useTranslation();
+  const isMobile = useDevice();
+  const [isOpen, setIsOpen] = useState(false);
+  const { isArticlesPageWasOpened } = useJsonSettings();
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (!isArticlesPageWasOpened) {
+      setIsOpen(true);
+      dispatch(saveJsonSettings({ isArticlesPageWasOpened: true })).catch(
+        (e) => {
+          console.log(e);
+        }
+      );
+    }
+  }, [dispatch, isArticlesPageWasOpened]);
+
+  const onClose = (): void => {
+    setIsOpen(false);
+  };
+
+  const text = (
+    <Text
+      title={t('Добро пожаловать на страницу статей')}
+      text={t(
+        'Здесь вы можете искать и просматривать статьи на различные темы'
+      )}
+    />
+  );
+
+  if (isMobile)
+    return (
+      <Drawer isOpen={isOpen} onClose={onClose}>
+        {text}
+      </Drawer>
+    );
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      {text}
+    </Modal>
+  );
+});
--- src/pages/ArticlePage/ui/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ui/ArticlePage/ArticlePage.tsx
@@ -14,6 +14,7 @@ import { ArticleInfiniteList } from '../ArticleInfiniteList/ArticleInfiniteList'
 
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './ArticlePage.module.scss';
+import { ArticlePageGreeting } from '@/features/ArticlePageGreeting';
 
 interface ArticlePageProps {
   className?: string;
@@ -46,6 +47,7 @@ const ArticlePage: FC<ArticlePageProps> = ({
       >
         <ArticlePageFilters />
         <ArticleInfiniteList className={cls.list} />
+        <ArticlePageGreeting />
       </Page>
     </DynamicModuleLoader>
   );
