56f1708c24f283b1757863ea77f44eee15c12d7b
Andrey Gudin <aeon150791@gmail.com>
2023-10-10 13:52:00 +0400

    feat: add toggleFeature component

---
+++ src/shared/lib/features/ToggleFeatures/ToggleFeatures.tsx
@@ -0,0 +1,18 @@
+import type { FC, ReactElement } from 'react';
+import { getFeatureFlag } from '../setGetFeatures';
+import { FeatureFlags } from '@/shared/types/featureFlags';
+
+interface ToggleFeaturesProps {
+  feature: keyof FeatureFlags;
+  on: ReactElement;
+  off: ReactElement;
+}
+
+export const ToggleFeatures: FC<ToggleFeaturesProps> = ({
+  feature,
+  off,
+  on
+}: ToggleFeaturesProps) => {
+  if (getFeatureFlag(feature)) return on;
+  return off;
+};
