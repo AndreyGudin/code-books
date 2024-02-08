7f40b4392acc2bea39fead8329477891c4f10493
Andrey Gudin <aeon150791@gmail.com>
2024-02-08 16:00:36 +0400

    feat: add mobile view for articles details page

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -20,6 +20,8 @@ import { ToggleFeatures } from '@/shared/lib/features';
 import { StickyContentLayout } from '@/shared/layouts/StickyContentLayout';
 import { DetailsContainer } from '../DetailsContainer/DetailsContainer';
 import { AdditionalInfoContainer } from '../AdditionalInfoContainer/AdditionalInfoContainer';
+import { useDevice } from '@/shared/hooks/useDevice';
+import { RetractablePanel } from '@/features/RetractablePanel';
 
 interface ArticleDetailsPageProps {
   className?: string;
@@ -34,6 +36,41 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = ({
 }: ArticleDetailsPageProps) => {
   const { t } = useTranslation('article-details');
   const { id } = useParams<{ id: string }>();
+  const isMobile = useDevice();
+
+  const mainContent = (
+    <StickyContentLayout
+      content={
+        <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+          <VStack gap="16" max>
+            <DetailsContainer />
+            <ArticleRating articleId={id ?? ''} />
+            <ArticleRecommendationsList />
+            <ArticleDetailsComments id={id ?? ''} />
+          </VStack>
+        </Page>
+      }
+      right={<AdditionalInfoContainer />}
+    />
+  );
+
+  const mobileContent = (
+    <>
+      <RetractablePanel fullHeight={false}>
+        <AdditionalInfoContainer />
+      </RetractablePanel>
+      <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+        <VStack gap="16" max>
+          <DetailsContainer />
+          <ArticleRating articleId={id ?? ''} />
+          <ArticleRecommendationsList />
+          <ArticleDetailsComments id={id ?? ''} />
+        </VStack>
+      </Page>
+    </>
+  );
+
+  const contentForFeature = isMobile ? mobileContent : mainContent;
 
   if (id === undefined) {
     return (
@@ -47,23 +84,7 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = ({
     <DynamicModuleLoader reducers={reducers}>
       <ToggleFeatures
         feature="isAppRedesigned"
-        on={
-          <StickyContentLayout
-            content={
-              <Page
-                className={classNames(cls.ArticleDetailsPage, {}, [className])}
-              >
-                <VStack gap="16" max>
-                  <DetailsContainer />
-                  <ArticleRating articleId={id} />
-                  <ArticleRecommendationsList />
-                  <ArticleDetailsComments id={id} />
-                </VStack>
-              </Page>
-            }
-            right={<AdditionalInfoContainer />}
-          />
-        }
+        on={contentForFeature}
         off={
           <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
             <VStack gap="16" max>
